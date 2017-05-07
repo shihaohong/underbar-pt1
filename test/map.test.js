7d820757ab267cabc4cb4b0a4e0e0b8945cdc436
Shi-Hao Hong <shihaohong94@gmail.com>
2017-05-07 10:46:53 -0700

    added a test for map() adding -desu to the end of each string

--- test/map.test.js
+++ test/map.test.js
@@ -6,4 +6,10 @@ describe('map()', () => {
     const mappedArr = _.map(arr, (el) => el * el);
     expect(mappedArr).toEqual([1, 4, 9, 16, 25]);
   });
+
+  it('maps through every string in the array and adds a \'-desu\' to the end of each string', () => {
+    const arr = ['kawaii', 'sou', 'watashiwa shihao'];
+    const mappedArr = _.map(arr, (el) => el + '-desu');
+    expect(mappedArr).toEqual(['kawaii-desu', 'sou-desu', 'watashiwa shihao-desu']);
+  });
 });

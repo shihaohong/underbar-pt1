9a2bd9c4d7756a41b081fc1a7c207e39d0d96f02
Shi-Hao Hong <shihaohong94@gmail.com>
2017-05-07 11:09:30 -0700

    added 3 tests to reduce() test suite

--- test/reduce.test.js
+++ test/reduce.test.js
@@ -13,4 +13,22 @@ describe('reduce()', () => {
     expect(result).toEqual(15);
   });
 
+  it('reduces an array of numbers to a multiple, *with* an explicit initial value for the accumulator', () => {
+    const nums = [1, 2, 3, 4, 5];
+    const result = _.reduce(nums, (acc, num) => acc * num, 2);
+    expect(result).toEqual(240);
+  });
+
+  it('reduces an array of numbers to a multiple, *without* an explicit initial value for the accumulator', () => {
+    const nums = [1, 2, 3, 4, 5];
+    const result = _.reduce(nums, (acc, num) => acc * num);
+    expect(result).toEqual(120);
+  });
+
+  it('reduces an array of strings into one concatenated string, *without* an explicit initial value for the accumulator', () => {
+    const strings = ['hello', 'its', 'me', 'Mario'];
+    const result = _.reduce(strings, (acc, strings) => acc + ' ' + strings);
+    expect(result).toEqual('hello its me Mario');
+  });
+
 });

cfa494baf2923a8bec3dee2ff6be57480d82465a
Shi-Hao Hong <shihaohong94@gmail.com>
2017-05-07 15:31:57 -0700

    added uniq() test to Test Suite

--- test/uniq.test.js
+++ test/uniq.test.js
@@ -5,4 +5,17 @@ describe('uniq()', () => {
     const nums = [2, 4, 5, 5, 7, 4, 10, 2];
     expect(_.uniq(nums)).toEqual([2, 4, 5, 7, 10]);
   });
+
+  it('de-dups a list of people', () => {
+    const people = {
+      'person1': 'Shi-Hao',
+      'person2': 'Ryan',
+      'person3': 'Steve',
+      'person4': 'Ryan',
+      'person5': 'Benjamin',
+      'person6': 'Steve'
+    }
+
+    expect(_.uniq(people)).toEqual(['Shi-Hao', 'Ryan', 'Steve', 'Benjamin']);
+  });
 });

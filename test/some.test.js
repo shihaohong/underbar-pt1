f17d43dee8f4c48d643d1f9f9fb6ad5c604eeed1
Shi-Hao Hong <shihaohong94@gmail.com>
2017-05-07 11:26:10 -0700

    updated tests in some() Test Suite

--- test/some.test.js
+++ test/some.test.js
@@ -11,4 +11,13 @@ describe('some()', () => {
     expect(_.some(nums, num => num % 2 === 1)).toBe(false);
   });
 
+  it('returns true if any element is not a number', () => {
+    const arr = [2, 4, 6, 'Not a number :)'];
+    expect(_.some(arr, value => isNaN(arr))).toBe(true);
+  });
+
+  it('returns false if any element is a number', () => {
+    const arr = ['hi', 'my', 'name', 'is', 5];
+    expect(_.some(arr, value => !isNaN(arr))).toBe(false);
+  });
 });
